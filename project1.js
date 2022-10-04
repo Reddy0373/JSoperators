6bec7516c558c30c0060c0facc830b1ede7786ba
Reddy0373 <sunilreddy9380@gmail.com>
2022-10-04 15:53:19 +0530

    Operators

---
+++ project1.js
@@ -0,0 +1,64 @@
+// Arithmatic op
+
+let a = 50
+let b = 25
+let c =a+b
+let c1 =a-b
+let c2 = a*b
+let c3 = a/b
+let c4 = a%b
+console.log(c,c1,c2,c3,c4);
+
+// Relationa op
+
+let d = 25
+let e = 40
+let f = d<e
+let f1 = d<=e
+let f2 = d>e
+let f3 = d>=e
+let f4 = d|=e
+console.log(f,f1,f2,f3,f4,);
+
+//Logical op
+let g = 24
+let h = 69
+let i = g&&h
+let i1 = g||h
+let i2 = g|h
+console.log(i,i1,i2);
+
+// Assignment op
+let a1=56
+let a2=45
+let a3=a1=a2
+console.log(a3);
+
+//Incremental/Decremental op
+
+let d1 =56
+++d1;
+console.log(d1);
+
+let d3=45
+d3--;
+console.log(d3);
+
+//conditional op
+let e1 = 26;
+let drink = e1 <= 56? "Beer" : "Juice";
+console.log(drink);
+
+//Bitwise op
+let z=23
+let y=68
+let x=z&y
+let x1=z|y
+let x2=z<<y
+let x3=z>>y
+console.log(x,x1,x2,x3);
+
+//Special op
+let g1= "Hello!"
+typeName=typeof(g1)
+console.log(typeName);
